16b7a43d9e3194bfa4c9e83f9a3b1b402999c8ac
etcdiary <r.mobi@hotmail.com>
2026-09-01 19:42:01 +0100

    fix: format bootstrap terraform

--- terraform/bootstrap/backend.tf
+++ terraform/bootstrap/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "platform-engineering-bootstrap-state"
-    key            = "terraform/bootstrap.tfstate"
-    region         = "eu-west-2"
-    encrypt        = true
+    bucket  = "platform-engineering-bootstrap-state"
+    key     = "terraform/bootstrap.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
   }
 }
 
