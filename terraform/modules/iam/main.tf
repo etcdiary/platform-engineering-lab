c5446b953ced265dd5f1a9c67072499f99068177
etcdiary <r.mobi@hotmail.com>
2026-09-03 23:43:00 +0100

    add dedicated lambda iam module

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "lambda_execution" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
